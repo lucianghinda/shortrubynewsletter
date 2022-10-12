836975090f8d79464607f6bfb277a41b7ed1873f
Lucian Ghinda <lucianghinda@users.noreply.github.com>
2022-10-12 20:42:36 +0300

    working with string - and string +

--- code-summaries/2022/week-41/string_plus_minus_on_string.rb
+++ code-summaries/2022/week-41/string_plus_minus_on_string.rb
@@ -24,3 +24,49 @@ puts "#{str4.object_id}, #{str4.frozen?}" # 100, true
 
 str5 = +"Normal string"
 puts "#{str5.object_id}, #{str5.frozen?}" # 120, false
+
+
+# Exploring some interesting cases
+
+hash1 = { "Key" => "Value" }
+key = hash1.keys[0]
+value = hash1.values[0]
+puts "H1: #{key.object_id}, #{value.object_id}" # 140, 160
+puts "H1: #{key.frozen?}, #{value.frozen?}" # true, false
+
+hash2 = { -"Key" => -"Value" }
+key = hash2.keys[0]
+value = hash2.values[0]
+puts "H2: #{key.object_id}, #{value.object_id}" # 140, 180
+puts "H2: #{key.frozen?}, #{value.frozen?}" # true, true
+
+# So if you want to use with Hash just add the - to the value
+hash3 = { "Key" => -"Value" }
+key = hash2.keys[0]
+value = hash2.values[0]
+puts "H3: #{key.object_id}, #{value.object_id}" # 140, 180
+puts "H3: #{key.frozen?}, #{value.frozen?}" # true, true
+
+
+# Some possible examples String#- could be useful
+
+## 1️⃣ When possible please use symbols and not strings
+
+## 2️⃣ When the string has a high chance of re-use
+
+## 3️⃣ When you think it is short to use -"string" then "string".freeze
+
+## Some examples
+
+# When writing a custom SQL that might be re-used in some other places
+SolarSystem.planets.order(-"orbit = 'circular' ASC, name ASC")
+
+# When calling an external API where you always define the URL
+base = -"http://example.com"
+url = URI.join(base, -"/foo")
+
+# When you specify the header
+header = { -"Content-Type" => -"application/x-www-form-urlencoded" }
+
+# When you log some structured short info
+Rails.logger(-"job.start.highpriority")
